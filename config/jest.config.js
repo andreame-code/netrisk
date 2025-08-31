df536fa5132201d5f6bd8481e6cee1ccc5f6e1ba
andreame-code <andrea.pasqualini.me@gmail.com>
2025-08-31 12:28:14 +0200

    Skip preview for forked PRs

--- config/jest.config.js
+++ config/jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   rootDir: path.resolve(__dirname, '..'),
   testEnvironment: 'jsdom',
   testMatch: ['**/*.test.[jt]s'],
+  setupFilesAfterEnv: ['<rootDir>/tests/jest.setup.js'],
   transform: {
     '^.+\\.[tj]s$': ['babel-jest', { configFile: path.resolve(__dirname, 'babel.config.js') }]
   }
