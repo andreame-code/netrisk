b7ea7a25bdadfd9854ba4cb8ade2562bdef5026c
andreame-code <andrea.pasqualini.me@gmail.com>
2025-09-09 23:35:48 +0200

    feat: set base path by mode

--- config/vite.config.js
+++ config/vite.config.js
@@ -1,34 +1,38 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
-export default defineConfig({
-  root: resolve(__dirname, '..'),
-  base: './',
-  build: {
-    polyfillModulePreload: false,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, '../index.html'),
-        about: resolve(__dirname, '../about.html'),
-        game: resolve(__dirname, '../game.html'),
-        lobby: resolve(__dirname, '../lobby.html'),
-        setup: resolve(__dirname, '../setup.html'),
-        howToPlay: resolve(__dirname, '../how-to-play.html'),
-        login: resolve(__dirname, '../login.html'),
-        forgot: resolve(__dirname, '../forgot.html'),
-        register: resolve(__dirname, '../register.html'),
-        account: resolve(__dirname, '../account.html'),
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development';
+
+  return {
+    root: resolve(__dirname, '..'),
+    base: isDev ? '/' : '/netrisk/',
+    build: {
+      polyfillModulePreload: false,
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, '../index.html'),
+          about: resolve(__dirname, '../about.html'),
+          game: resolve(__dirname, '../game.html'),
+          lobby: resolve(__dirname, '../lobby.html'),
+          setup: resolve(__dirname, '../setup.html'),
+          howToPlay: resolve(__dirname, '../how-to-play.html'),
+          login: resolve(__dirname, '../login.html'),
+          forgot: resolve(__dirname, '../forgot.html'),
+          register: resolve(__dirname, '../register.html'),
+          account: resolve(__dirname, '../account.html'),
+        },
       },
     },
-  },
-  resolve: {
-    extensions: ['.js', '.ts'],
-    alias: {
-      '@app': resolve(__dirname, '../src'),
-      '@game': resolve(__dirname, '../src/game'),
-      '@features': resolve(__dirname, '../src/features'),
-      '@shared': resolve(__dirname, '../src/shared'),
-      '@infra': resolve(__dirname, '../src/infra'),
+    resolve: {
+      extensions: ['.js', '.ts'],
+      alias: {
+        '@app': resolve(__dirname, '../src'),
+        '@game': resolve(__dirname, '../src/game'),
+        '@features': resolve(__dirname, '../src/features'),
+        '@shared': resolve(__dirname, '../src/shared'),
+        '@infra': resolve(__dirname, '../src/infra'),
+      },
     },
-  },
+  };
 });
