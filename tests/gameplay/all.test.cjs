c73fa755861d5f7762eab490309fe8298b23121e
andreame-code <228015774+andreame-code@users.noreply.github.com>
2026-04-12 17:26:29 +0200

    test: migrate gameplay runner to TypeScript

--- tests/gameplay/all.test.cjs
+++ tests/gameplay/all.test.cjs
@@ -1,14 +1,1 @@
-require("./setup/game-setup.test.cjs");
-require("./turn-flow/turn-flow.test.cjs");
-require("./reinforcement/reinforcement-calculation.test.cjs");
-require("./reinforcement/map-continent-bonuses.test.cjs");
-require("./reinforcement/reinforcement-placement.test.cjs");
-require("./combat/attack-validation.test.cjs");
-require("./combat/combat-resolution.test.cjs");
-require("./combat/banzai-attack.test.cjs");
-require("./conquest/conquest-resolution.test.cjs");
-require("./fortify/fortify-movement.test.cjs");
-require("./victory/victory-detection.test.cjs");
-require("./victory/elimination-and-victory.test.cjs");
-require("./regression/full-flows.test.cjs");
-
+module.exports = require('../../.tsbuild/tests/gameplay/all.test.cjs');
---
+++ tests/gameplay/all.test.cts
@@ -0,0 +1,19 @@
+const gameplayTestModules = [
+  "./setup/game-setup.test.cjs",
+  "./turn-flow/turn-flow.test.cjs",
+  "./reinforcement/reinforcement-calculation.test.cjs",
+  "./reinforcement/map-continent-bonuses.test.cjs",
+  "./reinforcement/reinforcement-placement.test.cjs",
+  "./combat/attack-validation.test.cjs",
+  "./combat/combat-resolution.test.cjs",
+  "./combat/banzai-attack.test.cjs",
+  "./conquest/conquest-resolution.test.cjs",
+  "./fortify/fortify-movement.test.cjs",
+  "./victory/victory-detection.test.cjs",
+  "./victory/elimination-and-victory.test.cjs",
+  "./regression/full-flows.test.cjs"
+];
+
+gameplayTestModules.forEach((modulePath) => {
+  require(modulePath);
+});
